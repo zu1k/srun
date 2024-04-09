c616942bf23d4d688686503657fa14fb7a419829
zu1k <i@zu1k.com>
2024-04-09 11:29:46 +0800

    fix(CI): format code

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
 use std::{env, net::Ipv4Addr};
 
 fn main() {
-    let auth_server_ip =
-        env!(
+    let auth_server_ip = env!(
         "AUTH_SERVER_IP",
         "Expect env AUTH_SERVER_IP, export AUTH_SERVER_IP=10.0.0.1"
     );
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 pub use file::read_config_from_file;
 pub use srun::*;
 pub use user::User;
-pub use utils::get_ip_by_if_name;
-pub use utils::select_ip;
+pub use utils::{get_ip_by_if_name, select_ip};
 pub use xencode::param_i;
 
-mod user;
-mod srun;
-mod xencode;
 mod file;
-mod utils;
 #[cfg(feature = "ureq")]
 mod http_client;
+mod srun;
+mod user;
+mod utils;
+mod xencode;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
